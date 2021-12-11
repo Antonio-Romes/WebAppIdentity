39b2f0c662b71f81c99e75b90a0be6b5f802e664
Antonio-Romes <antoniorome1@hotmail.com>
2021-12-11 01:30:04 -0300

    Finalizado a seção Identity e Entity Framework Core.

--- WebApp.Identity/Startup.cs
+++ WebApp.Identity/Startup.cs
@@ -2,12 +2,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace WebApp.Identity
@@ -26,9 +28,18 @@ namespace WebApp.Identity
         {
             services.AddControllersWithViews();
 
-            services.AddIdentityCore<IdentityUser>(options => { });
-            services.AddScoped<IUserStore<IdentityUser>,
-                UserOnlyStore<IdentityUser, IdentityDbContext>>();
+            var connectionString = @"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=IdentityCurso;Data Source=DESKTOP-U1RIFMR\SQLEXPRESS";
+            var migrationAssembly = typeof(Startup)
+                .GetTypeInfo().Assembly
+                .GetName().Name;
+
+            services.AddDbContext<MyUserDbContext>(
+                opt => opt.UseSqlServer(connectionString, sql => 
+                sql.MigrationsAssembly(migrationAssembly)));
+
+            services.AddIdentityCore<MyUser>(options => { });
+            services.AddScoped<IUserStore<MyUser>,
+                UserOnlyStore<MyUser, MyUserDbContext>>();
 
             services.AddAuthentication("cookies")
                 .AddCookie("cookies", options => options.LoginPath = "/Home/Login");
